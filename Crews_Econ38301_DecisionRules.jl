e9cb34bd6eddac3e1b41acb550da62c7021feb99
Levi Crews <levicrews24@gmail.com>
2019-02-14 12:50:45 -0600

    Update Crews_Econ38301_DecisionRules.jl

--- Crews_Econ38301_DecisionRules.jl
+++ Crews_Econ38301_DecisionRules.jl
@@ -1,6 +1,6 @@
 ## Crews_Econ38301_DecisionRules
 
-function Crews_Econ38001_DecisionRules(w::Float64, E::Array{Float64,2}, K::Array{Float64,2},
+function Crews_Econ38301_DecisionRules(w::Float64, E::Array{Float64,2}, K::Array{Float64,2},
     theta::Float64, nu::Float64, beta::Float64, delta::Float64, xi::Float64, thresh::Float64,
     p_LL::Float64, p_HH::Float64)
     #=
@@ -35,18 +35,18 @@ function Crews_Econ38001_DecisionRules(w::Float64, E::Array{Float64,2}, K::Array
     # Initialize =while= loop
     err = 100
     bound = 10^(-6)
-    V_1 = Crews_Econ38001_ValueFuncIter(Pi, Pi, E, K3, beta, delta, xi, thresh, p_LL, p_HH, w)[1]
+    V_1 = Crews_Econ38301_ValueFuncIter(Pi, Pi, E, K3, beta, delta, xi, thresh, p_LL, p_HH, w)[1]
 
     # =while= loop
     V_n1 = V_1
     while err > bound
-        V_n2 = Crews_Econ38001_ValueFuncIter(V_n1, Pi, E, K3, beta, delta, xi, thresh, p_LL, p_HH, w)[1]
+        V_n2 = Crews_Econ38301_ValueFuncIter(V_n1, Pi, E, K3, beta, delta, xi, thresh, p_LL, p_HH, w)[1]
         V_n = V_n1
         V_n1 = V_n2
         err = maximum(abs.(V_n1 - V_n))
     end
 
     # Get decision rule
-    H = Crews_Econ38001_ValueFuncIter(V_n1, Pi, E, K3, beta, delta, xi, thresh, p_LL, p_HH, w)[2]
+    H = Crews_Econ38301_ValueFuncIter(V_n1, Pi, E, K3, beta, delta, xi, thresh, p_LL, p_HH, w)[2]
     return H, p_LL, p_HH
 end
